79fbb8056d9f58f5d47f18cf60d84ec4c56dcdc1
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-06 00:16:29 +0530

    static typing for props in react with typescript with interface

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
-const TextField = () => {
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+interface Props {
+  id: number;
+  name: string;
+  add?: string;
+  salary?: number;
+  isValid: boolean;
+  fn: (bob: string) => string;
+  person: Person;
+}
+
+const TextField = (props: Props) => {
   return (
     <>
       <input />

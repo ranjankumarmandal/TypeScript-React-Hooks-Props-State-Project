c4ae1e8d7cebe65abaee81586478a3531c4f037d
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-06 10:26:58 +0530

    impolemented how to use useRef() hook in react with typescript

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 interface Person {
   firstName: string;
@@ -19,11 +19,15 @@ const TextField = (props: Props) => {
   const [count, setCount] = useState(''); // type inference automatically - this typing is best for state declaration in react
   const [num, setNum] = useState<number | undefined | null>(0);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
   return (
-    <>
+    <div ref={divRef}>
       <input type='text' onChange={(e) => setCount(e.target.value)} />
       <p>{count}</p>
-    </>
+      <input ref={inputRef} />
+    </div>
   );
 };
 

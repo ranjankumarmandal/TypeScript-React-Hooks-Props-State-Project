7c50df047a88bb395bd872dc622e4d0096292668
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-06 12:56:44 +0530

    created a component Counter_renderProps.tsx for using it in render props

--- src/components/Counter_renderProps.tsx
+++ src/components/Counter_renderProps.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Counter_renderProps = () => {
-  return <div>Counter_renderProps</div>;
+interface Props {
+  children: (
+    count: number,
+    setCount: React.Dispatch<React.SetStateAction<number>>
+  ) => JSX.Element | null;
+}
+
+const Counter_renderProps: React.FC<Props> = ({ children }) => {
+  const [count, setCount] = useState(0);
+
+  return <div>{children(count, setCount)}</div>;
 };
 
 export default Counter_renderProps;

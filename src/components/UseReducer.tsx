8d96dca5971378647cad204cd46fd7fc6dc3d70a
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-06 12:21:22 +0530

    implemented - In this way we can use useReducer() hook in typescript

--- src/components/UseReducer.tsx
+++ src/components/UseReducer.tsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+interface Action {
+  type: string;
+  payload: string;
+}
+interface S {
+  text: string;
+  complete: boolean;
+}
+type State = S[];
+
+const testReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'add':
+      return [...state, { text: action.payload, complete: false }];
+
+    case 'remove':
+      return state.filter((item) => item.text !== action.payload);
+
+    default:
+      return state;
+  }
+};
 
 const UseReducer = () => {
+  const [state, dispatch] = useReducer(testReducer, []);
+
   return <div>UseReducer</div>;
 };
 
